6984b2b828702aaa4bf14ea206d3f3803d657123
hekexun <hekexun@126.com>
2018-07-29 23:39:08 +0800

    712

--- src/main/webapp/Files/js/index.js
+++ src/main/webapp/Files/js/index.js
@@ -10,77 +10,58 @@ function indexinit(){
         $('#loginUser').html('user')
     }
     /////柱状图
-    app.title = '多 Y 轴示例';
-
-    var colors = ['#5793f3', '#d14a61', '#675bba'];
-
+    var ext=echarts.init(document.getElementById('carOnline'));///$("#ext1").get(0));
     option = {
-        color: colors,
-
-        tooltip: {
-            trigger: 'axis',
-            axisPointer: {
-                type: 'cross'
-            }
+        title : {
+            text: '近7天上线量',
+            subtext: 'Data From Catarc'
         },
-        grid: {
-            right: '20%'
-        },
-        toolbox: {
-            feature: {
-                dataView: {show: true, readOnly: false},
-                restore: {show: true},
-                saveAsImage: {show: true}
-            }
+        tooltip : {
+            trigger: 'axis'
         },
         legend: {
-            data:['蒸发量','降水量','平均温度']
+            data:['车辆数']
         },
-        xAxis: [
+        toolbox: {
+            show : true,
+            feature : {
+                dataView : {show: true, readOnly: false},
+                magicType : {show: true, type: ['line', 'bar']},
+                restore : {show: true},
+                saveAsImage : {show: true}
+            }
+        },
+        calculable : true,
+        xAxis : [
             {
-                type: 'category',
-                axisTick: {
-                    alignWithLabel: true
-                },
-                data: ['1','2','3','4','5','6','7']
+                type : 'category',
+                data : ['1','2','3','4','5','6','7']
             }
         ],
-        yAxis: [
+        yAxis : [
             {
-                type: 'value',
-                name: '上线车辆',
-                min: 0,
-                max: 25,
-                position: 'left',
-                axisLine: {
-                    lineStyle: {
-                        color: colors[2]
-                    }
+                type : 'value'
+            }
+        ],
+        series : [
+            {
+                name:'车辆数',
+                type:'bar',
+                data:[9, 11, 3, 5, 4, 33, 2],
+                markPoint : {
+                    data : [
+                        {type : 'max', name: '最大值'},
+                        {type : 'min', name: '最小值'}
+                    ]
                 },
-                axisLabel: {
-                    formatter: '{value} °C'
+                markLine : {
+                    data : [
+                        {type : 'average', name: '平均值'}
+                    ]
                 }
-            }
-        ],
-        series: [
-            {
-                name:'蒸发量',
-                type:'bar',
-                data:[2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3]
-            },
-            {
-                name:'降水量',
-                type:'bar',
-                yAxisIndex: 1,
-                data:[2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3]
-            },
-            {
-                name:'平均温度',
-                type:'line',
-                yAxisIndex: 2,
-                data:[2.0, 2.2, 3.3, 4.5, 6.3, 10.2, 20.3, 23.4, 23.0, 16.5, 12.0, 6.2]
             }
         ]
     };
 
+    ext.setOption(option);
 }
