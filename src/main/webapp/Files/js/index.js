7bfece32eca5a5fca9b5da51afaab2ede7c7e071
hekexun <hekexun@126.com>
2018-09-11 16:10:10 +0800

    712

--- src/main/webapp/Files/js/index.js
+++ src/main/webapp/Files/js/index.js
@@ -64,4 +64,59 @@ function indexinit(){
     };
 
     ext.setOption(option);
+    ////car mode echart
+    var extCarMode=echarts.init(document.getElementById('carOnline'));///$("#ext1").get(0));
+    optionCarMode = {
+        title : {
+            text: '近7天上线量',
+            subtext: 'Data From Catarc'
+        },
+        tooltip : {
+            trigger: 'axis'
+        },
+        legend: {
+            data:['车辆数']
+        },
+        toolbox: {
+            show : true,
+            feature : {
+                dataView : {show: true, readOnly: false},
+                magicType : {show: true, type: ['line', 'bar']},
+                restore : {show: true},
+                saveAsImage : {show: true}
+            }
+        },
+        calculable : true,
+        xAxis : [
+            {
+                type : 'category',
+                data : ['1','2','3','4','5','6','7']
+            }
+        ],
+        yAxis : [
+            {
+                type : 'value'
+            }
+        ],
+        series : [
+            {
+                name:'车辆数',
+                type:'bar',
+                data:[9, 11, 3, 5, 4, 33, 2],
+                markPoint : {
+                    data : [
+                        {type : 'max', name: '最大值'},
+                        {type : 'min', name: '最小值'}
+                    ]
+                },
+                markLine : {
+                    data : [
+                        {type : 'average', name: '平均值'}
+                    ]
+                }
+            }
+        ]
+    };
+
+    extCarMode.setOption(optionCarMode);
 }
